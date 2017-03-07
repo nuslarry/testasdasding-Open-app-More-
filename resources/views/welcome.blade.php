68a452b2c3bc247b8d8a8e43f9f15d2c9adf95e5
nuslarry <e0147977@u.nus.edu>
2017-03-07 21:58:21 +0800

    wtf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    wtf
                 </div>
 
                 <div class="links">
